d7274a887d593587554a8831273beb6170dcdfe4
Rendrikson Oliveira <40209027+Rendrikson98@users.noreply.github.com>
2020-10-09 10:00:23 -0300

    Adição da barra de podcasts e linhas dos podcast

--- mobile/src/pages/initialPage/index.js
+++ mobile/src/pages/initialPage/index.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Text, View } from 'react-native';
+import { MaterialCommunityIcons , Feather } from '@expo/vector-icons'; 
 
 import styles from './style';
 
@@ -12,8 +13,156 @@ export default function App() {
         <Text style={styles.logo}>Google Podcast</Text>
         <View style={styles.profile} />
       </View>
-      <Text>Teste</Text>
+      <View style={styles.podcastSubscribe} >
+        <View style={styles.podcast} />
+        <View style={styles.podcast} />
+        <View style={styles.podcast} />
+        <View style={styles.podcast} />
+        <View style={styles.podcast} />
+        <View style={styles.podcast} />
+        <View style={styles.podcast} />
+      </View>
+      <View style={[styles.rowDescription, styles.borderTop]}>
+        <View style={styles.podcastProfile}>
+          <View style={styles.logoPodcast} />
+          <View style={styles.infoPodcast}>
+            <Text style={styles.title}>Título do podcast</Text>
+            <Text style={styles.time}>6 horas</Text>
+          </View>
+        </View>
+        <Text style={styles.titlePodcast}>
+        Titulo do Podcast - Episódio 01 ...
+        </Text>
+        <Text style={styles.descriptionPodcast}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </Text>
+        <View style={styles.rowButtons}>
+          <View style={styles.firstbutton}>
+            <MaterialCommunityIcons name="play-circle-outline" size={22} color="#3380e6" />
+            <Text style={styles.minutes}>00min</Text>
+          </View>
+          <MaterialCommunityIcons name="playlist-plus" size={22} color="#3380e6" />
+          <Feather name="arrow-down-circle" size={22} color="#3380e6" style={{marginLeft: '15px'}} />
+        </View>
+      </View>
+      <View style={styles.rowDescription}>
+        <View style={styles.podcastProfile}>
+          <View style={styles.logoPodcast} />
+          <View style={styles.infoPodcast}>
+            <Text style={styles.title}>Título do podcast</Text>
+            <Text style={styles.time}>6 horas</Text>
+          </View>
+        </View>
+        <Text style={styles.titlePodcast}>
+        Titulo do Podcast - Episódio 01 ...
+        </Text>
+        <Text style={styles.descriptionPodcast}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </Text>
+        <View style={styles.rowButtons}>
+          <View style={styles.firstbutton}>
+            <MaterialCommunityIcons name="play-circle-outline" size={22} color="#3380e6" />
+            <Text style={styles.minutes}>00min</Text>
+          </View>
+          <MaterialCommunityIcons name="playlist-plus" size={22} color="#3380e6" />
+          <Feather name="arrow-down-circle" size={22} color="#3380e6" style={{marginLeft: '15px'}} />
+        </View>
+      </View>
+      <View style={styles.rowDescription}>
+        <View style={styles.podcastProfile}>
+          <View style={styles.logoPodcast} />
+          <View style={styles.infoPodcast}>
+            <Text style={styles.title}>Título do podcast</Text>
+            <Text style={styles.time}>6 horas</Text>
+          </View>
+        </View>
+        <Text style={styles.titlePodcast}>
+        Titulo do Podcast - Episódio 01 ...
+        </Text>
+        <Text style={styles.descriptionPodcast}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </Text>
+        <View style={styles.rowButtons}>
+          <View style={styles.firstbutton}>
+            <MaterialCommunityIcons name="play-circle-outline" size={22} color="#3380e6" />
+            <Text style={styles.minutes}>00min</Text>
+          </View>
+          <MaterialCommunityIcons name="playlist-plus" size={22} color="#3380e6" />
+          <Feather name="arrow-down-circle" size={22} color="#3380e6" style={{marginLeft: '15px'}} />
+        </View>
+      </View>
+      <View style={styles.rowDescription}>
+        <View style={styles.podcastProfile}>
+          <View style={styles.logoPodcast} />
+          <View style={styles.infoPodcast}>
+            <Text style={styles.title}>Título do podcast</Text>
+            <Text style={styles.time}>6 horas</Text>
+          </View>
+        </View>
+        <Text style={styles.titlePodcast}>
+        Titulo do Podcast - Episódio 01 ...
+        </Text>
+        <Text style={styles.descriptionPodcast}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </Text>
+        <View style={styles.rowButtons}>
+          <View style={styles.firstbutton}>
+            <MaterialCommunityIcons name="play-circle-outline" size={22} color="#3380e6" />
+            <Text style={styles.minutes}>00min</Text>
+          </View>
+          <MaterialCommunityIcons name="playlist-plus" size={22} color="#3380e6" />
+          <Feather name="arrow-down-circle" size={22} color="#3380e6" style={{marginLeft: '15px'}} />
+        </View>
+      </View>
+      <View style={styles.rowDescription}>
+        <View style={styles.podcastProfile}>
+          <View style={styles.logoPodcast} />
+          <View style={styles.infoPodcast}>
+            <Text style={styles.title}>Título do podcast</Text>
+            <Text style={styles.time}>6 horas</Text>
+          </View>
+        </View>
+        <Text style={styles.titlePodcast}>
+        Titulo do Podcast - Episódio 01 ...
+        </Text>
+        <Text style={styles.descriptionPodcast}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </Text>
+        <View style={styles.rowButtons}>
+          <View style={styles.firstbutton}>
+            <MaterialCommunityIcons name="play-circle-outline" size={22} color="#3380e6" />
+            <Text style={styles.minutes}>00min</Text>
+          </View>
+          <MaterialCommunityIcons name="playlist-plus" size={22} color="#3380e6" />
+          <Feather name="arrow-down-circle" size={22} color="#3380e6" style={{marginLeft: '15px'}} />
+        </View>
+      </View>
+      <View style={styles.rowDescription}>
+        <View style={styles.podcastProfile}>
+          <View style={styles.logoPodcast} />
+          <View style={styles.infoPodcast}>
+            <Text style={styles.title}>Título do podcast</Text>
+            <Text style={styles.time}>6 horas</Text>
+          </View>
+        </View>
+        <Text style={styles.titlePodcast}>
+        Titulo do Podcast - Episódio 01 ...
+        </Text>
+        <Text style={styles.descriptionPodcast}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </Text>
+        <View style={styles.rowButtons}>
+          <View style={styles.firstbutton}>
+            <MaterialCommunityIcons name="play-circle-outline" size={22} color="#3380e6" />
+            <Text style={styles.minutes}>00min</Text>
+          </View>
+          <MaterialCommunityIcons name="playlist-plus" size={22} color="#3380e6" />
+          <Feather name="arrow-down-circle" size={22} color="#3380e6" style={{marginLeft: '15px'}} />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
+    
+    
   );
 }
--- mobile/src/pages/initialPage/style.js
+++ mobile/src/pages/initialPage/style.js
@@ -8,6 +8,8 @@ export default StyleSheet.create({
         backgroundColor: '#fff',
         paddingHorizontal: '10px',
         paddingTop: Constants.statusBarHeight + 10,
+        margin: 0,
+        padding: 0
     },
 
     menu: {
@@ -28,5 +30,105 @@ export default StyleSheet.create({
         fontSize: '1em',
         fontWeight: 'bold',
         fontFamily:'"Sans-Serif Product Sans",Roboto,RobotoDraft,Arial,sans-serif'
+    },
+
+    podcastSubscribe: {
+        flexDirection: 'row',
+        marginTop: '15px',
+        marginBottom: '15px',
+        justifyContent:'center'
+    },
+
+    podcast: {
+        width: '60px',
+        height: '60px',
+        backgroundColor: '#5f6368',
+        borderRadius: '8px',
+        marginRight: '5px'
+        
+    },
+
+    rowDescription:{
+        width: '105%',
+        height: 'auto',
+        marginLeft: '-10px',
+        borderBottomWidth: '1px',
+        borderStyle: 'solid',
+        borderColor: '#A9A9A9',
+    },
+
+    borderTop: {
+        borderTopWidth: '1px',
+    },
+
+    podcastProfile: {
+        flexDirection: 'row',
+        marginBottom: '10px',
+        marginLeft: '20px',
+        marginTop: '10px'
+    },
+
+    logoPodcast: {
+        width: '40px',
+        height: '40px',
+        borderRadius: '8px',
+        backgroundColor: '#5f6368'
+    },
+
+    infoPodcast: {
+        marginLeft: '5px'
+    },
+
+    title: {
+        fontSize: '0.8em',
+        fontWeight: 'bold'
+    },
+
+    time:{
+        fontSize: '0.5',
+        fontWeight: '400',
+        color: '#5f6368'
+    },
+
+    titlePodcast:{
+        width: '90%',
+        fontSize: '1em',
+        fontWeight: 'bold',
+        marginLeft: '20px'
+    },
+
+    descriptionPodcast:{
+        width: '90%',
+        fontSize: '0.8em',
+        marginLeft: '20px'
+    },
+
+    rowButtons: {
+        flexDirection: 'row',
+        marginTop: '10px',
+        marginLeft: '20px',
+        marginBottom: '10px',
+        alignItems: 'center',
+    },
+
+    firstbutton: {
+        flexDirection: 'row',
+        width: '90px',
+        borderRadius: '20px',
+        padding: '3px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: '10px',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderColor: '#A9A9A9',
+        backgroundColor: 'transparent'
+    },
+
+    minutes: {
+        marginLeft: '5px',
+        fontSize: '0.8em',
+        fontWeight: '700'
     }
+
 })
